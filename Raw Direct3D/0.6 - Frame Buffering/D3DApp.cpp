6c646c506216d45b9a31fed98b997810fa8e6b18
henzoparahua <hnz.2894henzop19@gmail.com>
2026-04-17 15:15:31 -0300

    saving changes, gonna try somethingggg

--- Raw Direct3D/0.6 - Frame Buffering/D3DApp.cpp
+++ Raw Direct3D/0.6 - Frame Buffering/D3DApp.cpp
@@ -98,4 +98,93 @@ void D3DApp::LoadPipeline()
 	factory->MakeWindowAssociation(Win32App::GetHwnd(), DXGI_MWA_NO_ALT_ENTER) >> chk;
 	swap_chain.As(&m_swap_chain) >> chk;
 	m_frame_index = m_swap_chain->GetCurrentBackBufferIndex();
+
+	//	Descriptor Heaps
+	{
+		//	RTV
+		D3D12_DESCRIPTOR_HEAP_DESC rtv_heap_desc{};
+		rtv_heap_desc.NumDescriptors = num_frames;
+		rtv_heap_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		rtv_heap_desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		m_device->CreateDescriptorHeap(&rtv_heap_desc, IID_PPV_ARGS(&m_rtv_heap));
+
+		m_rtv_descriptor_size = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	}
+
+	//	Frame Resources
+	{
+		CD3DX12_CPU_DESCRIPTOR_HANDLE rtv_handle(m_rtv_heap->GetCPUDescriptorHandleForHeapStart());
+
+		//	RTV e Command Allocator para cada frame
+		for (UINT n{ 0 }; n < num_frames; n++)
+		{
+			m_swap_chain->GetBuffer(n, IID_PPV_ARGS(&m_render_targets[n])) >> chk;
+			m_device->CreateRenderTargetView(m_render_targets[n].Get(), nullptr, rtv_handle);
+			rtv_handle.Offset(1, m_rtv_descriptor_size);
+
+			m_device->CreateCommandAllocator(
+				D3D12_COMMAND_LIST_TYPE_DIRECT,
+				IID_PPV_ARGS(&m_command_allocators[n])
+			);
+		};
+	}
+}
+
+void D3DApp::LoadAssets()
+{
+	//	Criando Root Signature
+	{
+		D3D12_FEATURE_DATA_ROOT_SIGNATURE feature_data{};
+		static const D3D_ROOT_SIGNATURE_VERSION root_signature_levels[]{
+			D3D_ROOT_SIGNATURE_VERSION_1_2, D3D_ROOT_SIGNATURE_VERSION_1_1, D3D_ROOT_SIGNATURE_VERSION_1_0
+		};
+		for (auto level : root_signature_levels) {
+			feature_data.HighestVersion = level;
+
+			if (SUCCEEDED(m_device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &feature_data, sizeof(feature_data))))
+			{
+				break;
+			}
+		}
+
+		CD3DX12_ROOT_SIGNATURE_DESC root_signature_desc;
+		root_signature_desc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+		ComPtr<ID3DBlob> signature;
+		ComPtr<ID3DBlob> error;
+		D3D12SerializeRootSignature(&root_signature_desc, feature_data.HighestVersion, &signature, &error) >> chk;
+		m_device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_root_signature)) >> chk;
+	}
+
+	//	Criando o Pipeline State, shaders e coisa e tal
+	{
+		UINT8* vertex_shader_data_address{ nullptr };
+		UINT8* pixel_shader_data_address{ nullptr };
+		UINT vertex_shader_data_length{ 0 };
+		UINT pixel_shader_data_length{ 0 };
+
+		ReadDataFromFile(GetAssetFullPath(L"shaders_VSMain.cso").c_str(), &vertex_shader_data_address, &vertex_shader_data_length) >> chk;
+		ReadDataFromFile(GetAssetFullPath(L"shaders_PSMain.cso").c_str(), &pixel_shader_data_address, &pixel_shader_data_length) >> chk;
+
+		D3D12_INPUT_ELEMENT_DESC input_element_descs[]{
+			{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+			{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}
+		};
+
+		//	Descrição da PSO
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC pso_desc{};
+		pso_desc.InputLayout = { input_element_descs, _countof(input_element_descs) };
+		pso_desc.pRootSignature = m_root_signature.Get();
+		pso_desc.VS = CD3DX12_SHADER_BYTECODE(vertex_shader_data_address, vertex_shader_data_length);
+		pso_desc.PS = CD3DX12_SHADER_BYTECODE(pixel_shader_data_address, pixel_shader_data_length);
+		pso_desc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+		pso_desc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+		pso_desc.DepthStencilState.DepthEnable = FALSE;
+		pso_desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		pso_desc.NumRenderTargets = 1;
+		pso_desc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+		pso_desc.SampleDesc.Count = 1;
+
+		m_device->CreateGraphicsPipelineState(&pso_desc, IID_PPV_ARGS(&m_pipeline_state)) >> chk;
+	}
 }
